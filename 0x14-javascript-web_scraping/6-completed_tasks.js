7603e02566730ea2a40a33747613fd6faa395c7e
Timi-19 <104963678+Timi-19@users.noreply.github.com>
2023-04-26 21:51:39 +0100

    06

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,8 +3,8 @@
 const request = require('request');
 const url = process.argv[2];
 
-request(url, function (error, response, body) {
-  if (error) {
+request(url, function (err, response, body) {
+  if (err) {
     console.log(err);
   } else if (response.statusCode === 200) {
     const completed = {};

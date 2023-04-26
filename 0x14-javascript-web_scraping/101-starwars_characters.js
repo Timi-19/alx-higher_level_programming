4bfcb6fc551e5850dd81e922d703d4fb17a2f8fb
Timi-19 <104963678+Timi-19@users.noreply.github.com>
2023-04-26 21:47:44 +0100

    101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,17 +3,17 @@ const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
   if (!error) {
-    const chars= JSON.parse(body).chars;
-    printCharacters(chars, 0);
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
 
-function printCharacters (chars, index) {
-  request(chars[index], function (error, response, body) {
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
     if (!error) {
       console.log(JSON.parse(body).name);
-      if (index + 1 < chars.length) {
-        printCharacters(chars, index + 1);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
       }
     }
   });
